3ea655714a9b0bb5f96ee75965dc284cb21bec5a
Shaveko.Evgeny <shave87@bk.ru>
2019-08-08 18:37:50 +0300

    ДЗ 9: Обеспечение выполнения предусловий в тестах для удаления и модификации для групп и контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,13 @@
+from model.contact import Contact
 
 
 def test_del_contact(app):
+    if app.contact.count() == 0:
+        app.contact.add_contact()
+        app.contact.fill_contact_form(Contact(
+            firstname="Василий", lastname="Петрович", middlename="Шапошников", nickname="Shapka", address="dfgdfgsfafg",
+            company="ghkdflghd", home="dlkfghlsd", title="dfngdgnf", mobile="23", work="45", fax="67", email="werw",
+            email2="wer", email3="dsfsd", homepage="sdfsdf", bday="4", bmonth="May", byear="1990",
+            address2="sdfsdfasdfasdf", phone2="werawdfadf", notes="asdfasdfasdfasdf"))
+        app.contact.confirm()
     app.contact.delete_contact()
--- test/test_mdf_group.py
+++ test/test_mdf_group.py
@@ -3,4 +3,8 @@ from model.group import Group
 
 
 def test_mdf_group(app):
+    if app.group.count() == 0:
+        app.group.add_group()
+        app.group.fill_group_form(Group(name="test"))
+        app.group.confirm()
     app.group.edit_group(Group(name="Edit", header="", footer="Edit"))
