faa46f85bf24d07408813de5f544a219510651dc
TapokVozmezdiya <shave87@bk.ru>
2019-09-13 02:17:00 +0300

    Вышел на ДЗ 20. проблемы с функцией get_contact_not_in_group

--- check_db_connection..py
+++ check_db_connection..py
@@ -1,11 +1,35 @@
 import pymysql.cursors
+from fixture.orm import ORMFixture
+from model.group import Group
 
-connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    l = db.get_contact_not_in_group(Group(id="11"))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    connection.close()
+    pass # db.destroy()
+
+
+# from fixture.db import DbFixture
+#
+# db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+#
+# try:
+#     contacts = db.get_contact_list()
+#     for contact in contacts:
+#         print(contacts)
+#     print(len(contacts))
+# finally:
+#     db.destroy()
+
+
+# try:
+#     groups = db.get_group_list()
+#     for group in groups:
+#         print(group)
+#     print(len(groups))
+# finally:
+#     db.destroy()
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.add_group()
         app.group.fill_group_form(Group(name="ndfg", header="asdasd", footer="dasda"))
@@ -13,4 +13,6 @@ def test_delete_some_group(app, db):
     new_groups = db.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
