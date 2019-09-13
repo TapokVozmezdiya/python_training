8409ae24695764671b24538a2fc4e717e18b9616
Shaveko.Evgeny <shave87@bk.ru>
2019-09-13 18:40:31 +0300

    Ошибка Phantom object

--- check_db_connection..py
+++ check_db_connection..py
@@ -1,16 +1,15 @@
-import pymysql.cursors
 from fixture.orm import ORMFixture
 from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contact_not_in_group(Group(id="11"))
+    l = db.get_contacts_in_group(Group(id="76"))
     for item in l:
         print(item)
     print(len(l))
 finally:
-    pass # db.destroy()
+    pass  # db.destroy()
 
 
 # from fixture.db import DbFixture
--- fixture/orm.py
+++ fixture/orm.py
@@ -1,8 +1,6 @@
 from pony.orm import *
-from datetime import datetime
 from model.group import Group
 from model.contact import Contact
-from pymysql.converters import decoders
 
 
 class ORMFixture:
@@ -28,7 +26,7 @@ class ORMFixture:
                      lazy=True)
 
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.bind(provider='mysql', host=host, database=name, user=user, password=password)
         self.db.generate_mapping()
         sql_debug(True)
 
@@ -51,12 +49,12 @@ class ORMFixture:
         return self.convert_contact_to_models(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
 
     @db_session
-    def get_contact_in_group(self, group):
+    def get_contacts_in_group(self, group):
         orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
         return self.convert_contact_to_models(orm_group.contacts)
 
     @db_session
-    def get_contact_not_in_group(self, group):
+    def get_contacts_not_in_group(self, group):
         orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
         return self.convert_contact_to_models(
             select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
